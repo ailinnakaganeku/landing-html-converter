6aef07d1dfb932f3967f230fec5db8768af8262d
ailinnakaganeku <ailin.nakaganeku@gmail.com>
2024-12-10 17:42:33 +0100

    creating header component

--- app/page.tsx
+++ app/page.tsx
@@ -1,37 +1,13 @@
 import { Button } from "@/components/button";
 import { CodePreview } from "@/components/code-preview";
+import { Header } from "@/components/header";
 import { HowItWorks } from "@/components/how-it-works";
 
 export default function LandingPage() {
   return (
     <div className="min-h-screen bg-white">
       {/* Header */}
-      <header className="container mx-auto px-4 py-4 flex justify-between items-center">
-        <div className="flex flex-row items-start justify-start mq750:hidden">
-          <div className="bg-[#00FF9D] text-black flex flex-row items-center justify-center py-2 px-[7px]">
-            <h3 className="m-0 relative text-inherit leading-[120%] font-bold font-[inherit]">
-              HTML
-            </h3>
-          </div>
-          <div className="flex-1 bg-black flex flex-row items-center justify-center py-2 px-[7px] text-[#00FF9D]">
-            <h3 className="m-0 flex-1 relative text-inherit leading-[120%] font-bold font-[inherit]">
-              CONVERTER
-            </h3>
-          </div>
-        </div>
-        <div className="md:flex flex-row items-start justify-start hidden">
-          <div className="bg-[#00FF9D] text-black flex flex-row items-center justify-center py-2 px-[7px]">
-            <h3 className="m-0 relative text-inherit leading-[120%] font-bold font-[inherit]">
-              HTML
-            </h3>
-          </div>
-          <div className="flex-1 bg-black flex flex-row items-center justify-center py-2 px-[7px] text-[#00FF9D]">
-            <h3 className="m-0 flex-1 relative text-inherit leading-[120%] font-bold font-[inherit]">
-              CONVERTER
-            </h3>
-          </div>
-        </div>
-      </header>
+      <Header />
 
       {/* Hero Section */}
       <section className="container mx-auto px-4 py-16 text-center">
@@ -72,7 +48,7 @@ export default function LandingPage() {
       </div>
 
       {/* How It Works Section */}
-     <HowItWorks/>
+      <HowItWorks />
 
       {/* Footer */}
       <footer className="bg-black text-white py-16 rounded-t-[39px]">
